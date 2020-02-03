f28c94c721031024871f7e734451cc9be7c3ef8b
DKDG <hdg9400@gmail.com>
2020-02-03 20:04:14 +0900

    first commit

---
+++ build/linux-x86_64-normal-server-release/langtools/gensrc/com/sun/tools/jdeps/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.jdeps.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "1.8.0-internal-daegyu_2020_02_03_10_48-b00" },
+            { "jdk", "1.8.0" },
+            { "release", "1.8.0-internal" },
+        };
+    }
+}
